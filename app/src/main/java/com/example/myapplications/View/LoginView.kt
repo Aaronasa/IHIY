4240140de5616f5a1139acec3edb81b811a863b4
Aaron Asa <aaron.soelistiono22@gmail.com>
2025-10-22 09:27:57 +0700

    Update login veiw

--- app/src/main/java/com/example/myapplications/View/LoginView.kt
+++ app/src/main/java/com/example/myapplications/View/LoginView.kt
@@ -15,6 +15,14 @@ class LoginView {
         )
     }
 
+    @Composable
+    fun halo(name: String, modifier: Modifier = Modifier) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+    }
+
     @Preview(showBackground = true)
     @Composable
     fun GreetingPreview() {
