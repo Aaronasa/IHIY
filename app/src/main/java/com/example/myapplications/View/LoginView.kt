68cbcaa201f23ad62f51755bc8547450aa09b67c
Aaron Asa <aaron.soelistiono22@gmail.com>
2025-10-22 09:05:23 +0700

    Add LoginView with greeting composable
    Introduced LoginView class containing a Greeting composable and its preview for displaying a simple greeting message using Jetpack Compose.

---
+++ app/src/main/java/com/example/myapplications/View/LoginView.kt
@@ -0,0 +1,25 @@
+package com.example.myapplications.View
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.myapplications.ui.theme.MyApplicationsTheme
+
+class LoginView {
+    @Composable
+    fun Greeting(name: String, modifier: Modifier = Modifier) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun GreetingPreview() {
+        MyApplicationsTheme {
+            Greeting("Android")
+        }
+    }
+}
